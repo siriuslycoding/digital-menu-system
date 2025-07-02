3ed2c6c343d92bd6f08a968d53fc13d5d1c423ef
siriuslycoding <anushreekamath1@gmail.com>
2025-07-02 07:11:34 +0530

    add to cart functionality added

--- rishi.backend/routes/menu.js
+++ rishi.backend/routes/menu.js
@@ -3,6 +3,7 @@ const {
     getMenu,
     getItem
 } = require('../controllers/menuController');
+
 const router = express.Router();
 
 // fetching whole menu
@@ -11,13 +12,4 @@ router.get('/', getMenu);
 // fetch single item
 router.get('/:id', getItem);
 
-// router.get('/',(req,res) => {
-//     res.json({msg:"Menu fetched!"})
-// })
-// router.get('/',(req,res) => {
-//     res.json({msg:"Menu fetched!"})
-// })
-// router.get('/',(req,res) => {
-//     res.json({msg:"Menu fetched!"})
-// })
 module.exports = router;
--- rishi.backend/server.js
+++ rishi.backend/server.js
@@ -1,17 +1,26 @@
 require('dotenv').config();
 const express = require('express');
-const menuRoutes = require('./routes/menu');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+const menuRoutes = require('./routes/menu');
+const cartRoutes = require('./routes/cart');
 
 const app = express();
+
+app.use(cors());
+
 // middleware
 app.use(express.json());
+
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
-})
+});
 //routes
 app.use('/api/menu', menuRoutes);
+app.use('/api/bill', cartRoutes);
+
 mongoose.connect(process.env.URI)
     .then(() => {
         app.listen(process.env.PORT, () => {
